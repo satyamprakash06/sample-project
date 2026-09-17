d0577b61434ce68e1b09e8a9c7bdf9a8603d6d7e
satyamprakash06 <satyam060104@gmail.com>
2026-09-18 01:08:50 +0530

    03 test sample

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get the hell started</h1>
+          <h1>Get it started</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
@@ -26,7 +26,7 @@ function App() {
           className="counter"
           onClick={() => setCount((count) => count + 1)}
         >
-          Count is {count}
+          Current Count is {count}
         </button>
       </section>
 
